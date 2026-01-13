25837f6c9ebcdd8b72796f7f5b645b2ba3f00af1
github guddlrdl123 <guddlrdl123@gmail.com>
2026-01-13 18:27:31 +0900

    고객 관리

--- customerprogram/dto/Customer.java
+++ customerprogram/dto/Customer.java
@@ -6,15 +6,17 @@ public class Customer {
     private char gender;
     private String email;
     private int birthYear;
+    private String rank;
 
     public Customer() {
     }
 
-    public Customer(String name, char gender, String email, int birthYear) {
+    public Customer(String name, char gender, String email, int birthYear, String rank) {
         this.name = name;
         this.gender = gender;
         this.email = email;
         this.birthYear = birthYear;
+        this.rank = rank;
     }
 
     public String getName() {
@@ -49,9 +51,62 @@ public class Customer {
         this.birthYear = birthYear;
     }
 
-    @Override
-    public String toString() {
-        return "Customer [name=" + name + ", gender=" + gender + ", email=" + email + ", birthYear=" + birthYear + "]";
+    public String getRank() {
+        return rank;
     }
 
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [name=" + name + ", gender=" + gender + ", email=" + email + ", birthYear=" + birthYear
+                + ", rank=" + rank + "]";
+    }
 }
+/*
+ * class Vip extends Customer {
+ * 
+ * public Vip(String name, char gender, String email, int birthYear, String
+ * rank) {
+ * super(name, gender, email, birthYear, rank);
+ * }
+ * 
+ * @Override
+ * public String toString() {
+ * return String.format("이름 : %s, 성별 : %s, 이메일 : %s, 출생년도 : %d, 회원등급: %s",
+ * getName(), getGender(), getEmail(),
+ * getBirthYear(), getRank());
+ * }
+ * 
+ * }
+ * 
+ * class Normal extends Customer {
+ * public Normal(String name, char gender, String email, int birthYear, String
+ * rank) {
+ * super(name, gender, email, birthYear, rank);
+ * }
+ * 
+ * @Override
+ * public String toString() {
+ * return String.format("이름 : %s, 성별 : %s, 이메일 : %s, 출생년도 : %d, 회원등급: %s",
+ * getName(), getGender(), getEmail(),
+ * getBirthYear(), getRank());
+ * }
+ * }
+ * 
+ * class Caution extends Customer {
+ * public Caution(String name, char gender, String email, int birthYear, String
+ * rank) {
+ * super(name, gender, email, birthYear, rank);
+ * }
+ * 
+ * @Override
+ * public String toString() {
+ * return String.format("이름 : %s, 성별 : %s, 이메일 : %s, 출생년도 : %d, 회원등급: %s",
+ * getName(), getGender(), getEmail(),
+ * getBirthYear(), getRank());
+ * }
+ * }
+ */
